f9142c2c228ec617ed4507977ed748ce152c35cd
twil1307 <totenduc1307@gmail.com>
2022-10-07 14:51:34 +0700

    update chart and components

--- src/pages/HotelManageDetail/components/Income/components/SelectBox/SelectBox.js
+++ src/pages/HotelManageDetail/components/Income/components/SelectBox/SelectBox.js
@@ -1,26 +1,64 @@
+import { useState } from "react";
 import SelectBoxStyle from "./SelectBox.module.scss";
 
-function SelectBox() {
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const monthsKey = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+function SelectBox({ onChangeMonth }) {
+  const [month, setMonth] = useState("");
+
+  const monthSelect = (e) => {
+    setMonth(e.target.value);
+    onChangeMonth(e.target.value);
+  };
+
   return (
     <div className={SelectBoxStyle["select-wrapper"]}>
       <label htmlFor="months" className={SelectBoxStyle["months-label"]}>
         Chọn một tháng để xem doanh thu
       </label>
       <div className={SelectBoxStyle["styled-select"]}>
-        <select name="months" id="months" className={SelectBoxStyle["months"]}>
+        <select
+          name="months"
+          id="months"
+          onChange={monthSelect}
+          className={SelectBoxStyle["months"]}
+        >
           <option value="">Chọn một tháng</option>
-          <option value="january">January</option>
-          <option value="february">February</option>
-          <option value="march">March</option>
-          <option value="april">April</option>
-          <option value="may">May</option>
-          <option value="june">June</option>
-          <option value="july">July</option>
-          <option value="august">August</option>
-          <option value="september">September</option>
-          <option value="october">October</option>
-          <option value="november">November</option>
-          <option value="december">December</option>
+          {months.map((data, index) => {
+            return (
+              <option value={monthsKey[index]} key={index}>
+                {data}
+              </option>
+            );
+          })}
         </select>
       </div>
     </div>
---
+++ src/pages/HotelManageDetail/components/Rating/Rating.js
@@ -0,0 +1,7 @@
+import RatingStyle from "./Chart.module.scss";
+
+function Rating() {
+  return <div>Rating</div>;
+}
+
+export default Rating;
