67eccecda04ff1d4fb867efa1a727582943b329f
twil1307 <totenduc1307@gmail.com>
2022-10-27 13:59:45 +0700

    backup

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -5,84 +5,108 @@ import registerStyles from "./Register.module.scss";
 import { useState, useMemo, Suspense } from "react";
 import tabs from "./tabs";
 import {
-    basicHotelInforInitState,
-    roomInfoInitState,
-    extraInforInitState,
+  basicHotelInforInitState,
+  roomInfoInitState,
+  extraInforInitState,
 } from "./registerInitStates";
 
 function Register() {
-    // show BasicInformation first
-    const [inputTabIndex, setInputTabIndex] = useState(0);
-    const [basicHotelInfor, setBasicHotelInfo] = useState(
-        basicHotelInforInitState
-    );
-    const [isNextTabValid, setNextTabValid] = useState(false);
-    const [amenities, setAmenities] = useState([]);
-    const [roomInfor, setRoomInfor] = useState(roomInfoInitState);
-    const [viewImages, setViewImages] = useState();
-    const [roomImages, setRoomImages] = useState();
-    const [backgroundImage, setBackgroundImage] = useState();
-    const [extraInfor, setExtraInfor] = useState(extraInforInitState);
-    const registerContextValue = useMemo(
-        () => ({
-            basicHotelInfor,
-            setBasicHotelInfo,
-            amenities,
-            setAmenities,
-            roomInfor,
-            setRoomInfor,
-            viewImages,
-            setViewImages,
-            roomImages,
-            setRoomImages,
-            backgroundImage,
-            setBackgroundImage,
-            extraInfor,
-            setExtraInfor,
-        }),
-        [
-            basicHotelInfor,
-            amenities,
-            roomInfor,
-            viewImages,
-            roomImages,
-            backgroundImage,
-            extraInfor,
-        ]
-    );
+  // show BasicInformation first
+  const [inputTabIndex, setInputTabIndex] = useState(0);
+  const [basicHotelInfor, setBasicHotelInfo] = useState(
+    basicHotelInforInitState
+  );
+  const [isNextTabValid, setNextTabValid] = useState(false);
+  const [amenities, setAmenities] = useState([]);
+  const [roomInfor, setRoomInfor] = useState(roomInfoInitState);
+  const [viewImages, setViewImages] = useState();
+  const [roomImages, setRoomImages] = useState();
+  const [backgroundImage, setBackgroundImage] = useState();
+  const [extraInfor, setExtraInfor] = useState(extraInforInitState);
+  const registerContextValue = useMemo(
+    () => ({
+      basicHotelInfor,
+      setBasicHotelInfo,
+      amenities,
+      setAmenities,
+      roomInfor,
+      setRoomInfor,
+      viewImages,
+      setViewImages,
+      roomImages,
+      setRoomImages,
+      backgroundImage,
+      setBackgroundImage,
+      extraInfor,
+      setExtraInfor,
+    }),
+    [
+      basicHotelInfor,
+      amenities,
+      roomInfor,
+      viewImages,
+      roomImages,
+      backgroundImage,
+      extraInfor,
+    ]
+  );
 
-    console.log('basic-form re-render')
+  //   console.log("basic-form re-render");
 
-    return (
-        <RegisterContext.Provider value={registerContextValue}>
-            <div id={registerStyles["register"]}>
-                <Grid container>
-                    <Grid item xs={4} className={registerStyles["left"]}>
-                        <Jumbotron />
-                    </Grid>
-                    <Grid item xs={8} className={registerStyles["right"]}>
-                        <Box
-                            sx={{
-                                width: '50%',
-                                margin: '0 auto',
-                                paddingTop: '10em'
-                            }}
-                        >
-                            <Suspense fallback={<div>Loading...</div>}>
-                                {tabs[inputTabIndex].render(setNextTabValid)}
-                            </Suspense>
-                        </Box>
-                        <TabBar
-                            inputTabIndex={inputTabIndex}
-                            handleChangeTab={setInputTabIndex}
-                            tabIdList={tabs.map(({ id }) => id)}
-                            isNextTabValid={isNextTabValid}
-                        />
-                    </Grid>
-                </Grid>
-            </div>
-        </RegisterContext.Provider>
+  const registerSubmit = (e) => {
+    e.preventDefault();
+    const hotelForm = new FormData();
+    // hotelForm.append("", )
+
+    console.log(
+      // basicHotelInfor
+      amenities
+      // roomInfor
+      // viewImages,
+      // roomImages,
+      // backgroundImage,
+      // extraInfor
     );
+  };
+
+  return (
+    <RegisterContext.Provider value={registerContextValue}>
+      <div id={registerStyles["register"]}>
+        <Grid container>
+          <Grid item xs={4} className={registerStyles["left"]}>
+            <Jumbotron />
+          </Grid>
+          <Grid item xs={8} className={registerStyles["right"]}>
+            <Box
+              sx={{
+                width: "50%",
+                margin: "0 auto",
+                paddingTop: "10em",
+              }}
+            >
+              <Suspense fallback={<div>Loading...</div>}>
+                {tabs[inputTabIndex].render(setNextTabValid)}
+              </Suspense>
+            </Box>
+            <TabBar
+              inputTabIndex={inputTabIndex}
+              handleChangeTab={setInputTabIndex}
+              tabIdList={tabs.map(({ id }) => id)}
+              isNextTabValid={isNextTabValid}
+            />
+            <button
+              className={registerStyles["btn-submit"]}
+              onClick={(e) => {
+                registerSubmit(e);
+              }}
+            >
+              Submit
+            </button>
+          </Grid>
+        </Grid>
+      </div>
+    </RegisterContext.Provider>
+  );
 }
 
 export default Register;
--- src/pages/Register/components/AmenityInformation/AmenityInformation.js
+++ src/pages/Register/components/AmenityInformation/AmenityInformation.js
@@ -6,48 +6,55 @@ import { RegisterContext } from "@/utils/contexts";
 import { useContext, useEffect, useState } from "react";
 
 const AmenityCard = ({ amenity, setAmenities }) => {
-    const handleOnClick = (e) => {
-        setAmenities(prev => {
-            const isIncluded = prev.some(({ id }) => id === amenity.id);
-            if (isIncluded) {
-                return prev.filter(({ id }) => id !== amenity.id)
-            } else {    
-                return [...prev, amenity]
-            }
-        })
-    }
+  const handleOnClick = (e) => {
+    setAmenities((prev) => {
+      const isIncluded = prev.some(({ id }) => id === amenity.id);
+      if (isIncluded) {
+        return prev.filter(({ id }) => id !== amenity.id);
+      } else {
+        return [...prev, amenity];
+      }
+    });
+  };
 
-    return (
-        <div key={amenity.id} onClick={handleOnClick}>
-            <FontAwesomeIcon icon={amenity.icon} />
-            {amenity.title}
-        </div>
-    );
+  return (
+    <div key={amenity.id} onClick={handleOnClick}>
+      <FontAwesomeIcon icon={amenity.icon} />
+      {amenity.title}
+    </div>
+  );
 };
 
 function AmenityInformation() {
-    const { amenities, setAmenities } = useContext(RegisterContext);
-    const [displayAmenities, setDisplayAmenities] = useState(defaultAmenities);
+  const { amenities, setAmenities } = useContext(RegisterContext);
+  const [displayAmenities, setDisplayAmenities] = useState(defaultAmenities);
 
-    useEffect(() => {
-        setAmenities(defaultAmenities)
-    }, [])
+  useEffect(() => {
+    setAmenities(defaultAmenities);
+  }, []);
 
-    return (
-        <div id={amenityInforStyles["basic-information"]}>
-            <div className={amenityInforStyles["basic-infor-header"]}>
-                Hãy cho khách hàng biết về những tiện nghi của khách sạn
-            </div>
-            <div className={amenityInforStyles["basic-infor-body"]}>
-                {displayAmenities.map((amenity) => (
-                    <AmenityCard amenity={amenity} setAmenities={setAmenities}/>
-                ))}
-                <div>
-                    <AmenityInputField handleClick={setAmenities} addNewAmenity={setDisplayAmenities}/>
-                </div>
-            </div>
+  return (
+    <div id={amenityInforStyles["basic-information"]}>
+      <div className={amenityInforStyles["basic-infor-header"]}>
+        Hãy cho khách hàng biết về những tiện nghi của khách sạn
+      </div>
+      <div className={amenityInforStyles["basic-infor-body"]}>
+        {displayAmenities.map((amenity, index) => (
+          <AmenityCard
+            amenity={amenity}
+            setAmenities={setAmenities}
+            key={index}
+          />
+        ))}
+        <div>
+          <AmenityInputField
+            handleClick={setAmenities}
+            addNewAmenity={setDisplayAmenities}
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default AmenityInformation;
