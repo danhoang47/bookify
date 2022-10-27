77cafd05d761630ce69e9e77dffc34839b601ab2
twil1307 <totenduc1307@gmail.com>
2022-10-27 13:02:34 +0700

    updateScript

--- src/main/java/model/dto/Room.java
+++ src/main/java/model/dto/Room.java
@@ -11,26 +11,19 @@ package model.dto;
 public class Room {
     private String room_id;
     private String hotel_id;
-    private String room_num;
     private double roomPrice;
-    private boolean avaiable;
+    private int type;
+    private int max_guest;
 
-    public Room(String room_id, String hotel_id, String room_num, double roomPrice, boolean avaiable) {
+    public Room(String room_id, String hotel_id, double roomPrice, int type, int max_guest) {
         this.room_id = room_id;
         this.hotel_id = hotel_id;
-        this.room_num = room_num;
         this.roomPrice = roomPrice;
-        this.avaiable = avaiable;
+        this.type = type;
+        this.max_guest = max_guest;
     }
-    
-    public Room(String room_num, double roomPrice, boolean avaiable) {
-        this.room_num = room_num;
-        this.roomPrice = roomPrice;
-        this.avaiable = avaiable;
-    }
-    
+
     public Room() {
-        
     }
 
     public String getRoom_id() {
@@ -49,14 +42,6 @@ public class Room {
         this.hotel_id = hotel_id;
     }
 
-    public String getRoom_num() {
-        return room_num;
-    }
-
-    public void setRoom_num(String room_num) {
-        this.room_num = room_num;
-    }
-
     public double getRoomPrice() {
         return roomPrice;
     }
@@ -65,18 +50,28 @@ public class Room {
         this.roomPrice = roomPrice;
     }
 
-    public boolean isAvaiable() {
-        return avaiable;
+    public int getType() {
+        return type;
     }
 
-    public void setAvaiable(boolean avaiable) {
-        this.avaiable = avaiable;
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getMax_guest() {
+        return max_guest;
+    }
+
+    public void setMax_guest(int max_guest) {
+        this.max_guest = max_guest;
     }
 
     @Override
     public String toString() {
-        return "Room{" + "room_id=" + room_id + ", hotel_id=" + hotel_id + ", room_num=" + room_num + ", roomPrice=" + roomPrice + ", avaiable=" + avaiable + '}';
+        return "Room{" + "room_id=" + room_id + ", hotel_id=" + hotel_id + ", roomPrice=" + roomPrice + ", type=" + type + ", max_guest=" + max_guest + '}';
     }
+
+    
     
     
 }
